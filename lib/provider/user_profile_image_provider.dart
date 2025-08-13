318646b0b8422a8c719be93bc638f2a73007ac8f
Prothes Barai <aspprothesbarai1619@gmail.com>
2025-08-13 17:24:52 +0600

    asp

--- lib/provider/user_profile_image_provider.dart
+++ lib/provider/user_profile_image_provider.dart
@@ -13,21 +13,28 @@ class UserProfileImageProvider extends ChangeNotifier{
     _init();
   }
 
+
+
+  Future<void> setProfileImage(File image) async{
+    _profileImage = image;
+    await _storeUserImage.put("store_user_image", image.path);
+    notifyListeners();
+  }
+
+
+
   Future<void> _init() async{
     _storeUserImage = Hive.box("StoreUserImage");
-    final storeImage = _storeUserImage.get("store_user_image");
-    if(storeImage != null && storeImage is File){
-      _profileImage = storeImage;
-      notifyListeners();
+    final path = _storeUserImage.get("store_user_image_path");
+    if (path != null && path is String) {
+      final file = File(path);
+      if (await file.exists()) {
+        _profileImage = file;
+        notifyListeners();
+      }
     }
   }
 
-  
-  Future<void> setProfileImage(File image) async{
-    _profileImage = image;
-    await _storeUserImage.put("store_user_image", image);
-    notifyListeners();
-  }
 
 
   Future<void> clearProfileImage() async {
